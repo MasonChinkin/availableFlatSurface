a3e1459c5f9f5a24f4690a9d1716a9c25cdd3211
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-26 14:14:59 -0800

    write configureStore and import to entry file

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById('root')
-  // let store = configureStore()
+  let store = configureStore
 
   ReactDOM.render(
     <h1>Working?</h1>,
---
+++ frontend/store/store.js
@@ -0,0 +1,17 @@
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from '../reducers/rootReducer';
+
+const configureStore = (preloadedState = {}) => {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(thunk)
+  );
+};
+
+export default configureStore;
