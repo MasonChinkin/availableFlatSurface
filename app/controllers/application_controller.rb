af80b2121a08bcedccac46408c04858a0f37255a
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-26 11:34:28 -0800

    Write application controller auth pattern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :require_sign_in
+
+  def sign_in(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token
+  end
+
+  def sign_out
+    current_user.reset_session_token
+    session[:session_token] = nil
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  # return status 403 Forbidden if user is unauthorized
+  def require_sign_in
+    render status: 403
+  end
 end
