8986307de3a8c38add4cd728b26cde56471ed1dc
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-26 11:50:14 -0800

    Write session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      sign_in(user)
+      render "api/users/show"
+    else
+      render json: ["Invalid username and/or password"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      sign_out
+      render "api/users/show"
+    else
+      render json: ["Nobody is signed in"], status: 404
+    end
+  end
 end
