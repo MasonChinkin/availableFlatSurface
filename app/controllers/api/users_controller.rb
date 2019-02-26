03fa8abced946694803b20fd837b4efb6abde2fd
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-26 12:02:34 -0800

    Write User controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,25 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(user)
+      render json: "api/users/show"
+    else
+      render json :@user.full_messages, status: 422
+    end
+  end
+
+  # bonus feature: edit user account
+  # def edit
+  # end
+
+  # bonus feature: delete user account
+  # def destroy
+  # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, format: {default: :json} do
-    resources :users, only: [:create, :edit, :destroy]
+    resources :users, only: [:create, :edit, :destroy] #edit and destroy are bonus features
     resource :session, only: [:create, :destroy]
   end
 end
