57a7984f60e78f09d4439884e842dc9297c6c256
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-26 10:47:12 -0800

    Migrate users table and write user model auth pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  name            :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  vip             :boolean          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :name, :email, :password_digest, :session_token, :vip, presence: true
+  validates :vip, inclusion: {in: [true, false]}
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user if user && user.is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandon::urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandon::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
