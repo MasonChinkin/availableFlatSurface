ce81bfc03c05d55e57420a6366c2b72203989bd5
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-27 10:13:47 -0800

    Authed/NotAuthed routes implemented in routeUtils

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import SignUpFormContainer from './session/SignupFormContainer';
 import { Route } from 'react-router-dom'
 import SigninFormContainer from './session/SigninFormContainer';
+import { AuthedRoute, NotAuthedRoute } from '../utils/routesUtil';
 
 const App = () => (
   <div>
-    <Route path={`/signup`} component={SignUpFormContainer} />
-    <Route path={`/signin`} component={SigninFormContainer} />
+    <AuthedRoute path={`/signup`} component={SignUpFormContainer} />
+    <AuthedRoute path={`/signin`} component={SigninFormContainer} />
   </div>
 )
 
