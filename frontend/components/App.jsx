923d9f6946045cee84c6e26a6a11baa716118849
Mason Chinkin <mason.chinkin@gmail.com>
2019-02-27 20:15:40 -0800

    Placehold splash screen

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,10 +5,12 @@ import SigninFormContainer from './session/SigninFormContainer';
 import { AuthedRoute, NotAuthedRoute } from '../utils/routesUtil';
 import NavBarContainer from './nav/NavBarContainer';
 import Footer from './footer/Footer';
+import BackgroundImage from './splash/ResForm/BackgroundImage';
 
 const App = () => (
   <>
     <Route path={`/`} component={NavBarContainer} />
+    <Route path={`/`} component={BackgroundImage} />
     <NotAuthedRoute path={`/signup`} component={SignUpFormContainer} />
     <NotAuthedRoute path={`/signin`} component={SigninFormContainer} />
     <Route path={`/`} component={Footer} />
---
+++ frontend/components/splash/ResForm/BackgroundImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BackgroundImage = () => {
+
+  return (
+    <div className="background-image-placeholder">
+      <p>
+        There will be stuff here. Promise.
+      </p>
+    </div>
+  )
+}
+
+export default BackgroundImage;
