8d11903c44a9515ce4b37198a0c7db2ddad7fa65
Mason Chinkin <mason.chinkin@gmail.com>
2019-03-05 16:27:27 -0800

    Render reservations

--- frontend/components/profile/ProfileReservationItem.jsx
+++ frontend/components/profile/ProfileReservationItem.jsx
@@ -4,13 +4,15 @@ class ProfileReservationItem extends Component {
   render() {
     let rest = this.props.restaurant;
     let res = this.props.reservation;
+    let resDate = new Date(res.reservation);
+    let date = resDate.getDate() + '/' + (resDate.getMonth() + 1) + '/' + resDate.getFullYear();
 
     return (
       <div className="profile-reservation-item">
         <img src={rest.profilePhotoURL} alt="restaurant photo" />
         <div>
           <h2>{rest.name}</h2>
-          <h2>{res.reservation}</h2>
+          <h2>{date}</h2>
           <h3>Table for {res.numPeople} people</h3>
         </div>
       </div>
--- frontend/components/profile/ProfileReservations.jsx
+++ frontend/components/profile/ProfileReservations.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import ProfileReservationItem from './ProfileReservationItem';
 
 class ProfileReservations extends Component {
   render() {
-    if (this.props.restaurants === undefined) return null;
-    debugger
+    let res = Object.values(this.props.reservations);
+    let rests = this.props.restaurants;
+    if (rests === undefined) return null;
+
+    // yes...this is iterating twice over everything.
+    let upcoming = res.map(res => {
+      if (new Date(res.reservation) > new Date()) {
+        return <ProfileReservationItem key={res.id} reservation={res} restaurant={rests[res.restaurantId]} />
+      }
+    })
+
+    let past = res.map(res => {
+      if (new Date(res.reservation) < new Date()) {
+        return <ProfileReservationItem key={res.id} reservation={res} restaurant={rests[res.restaurantId]} />
+      }
+    })
+
     return (
       <section className="reservations">
-        <div>
-          <h2>Upcoming Reservations</h2>
-        </div>
-        <div>
-          <h2>Past Reservations</h2>
-        </div>
+        <h2>Upcoming Reservations</h2>
+        {upcoming}
+        <h2>Past Reservations</h2>
+        {past}
       </section>
     );
   }
